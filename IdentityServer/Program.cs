180136eb083a3b1c2af0e01ec74b524d2761885f
Sea Oater <seaoater.pymath@gmail.com>
2023-06-13 21:27:01 +0200

    build: Add aspnet identity tables and migration.

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,6 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Reflection;
+using IdentityServer.Data;
+using IdentityServer.Factories;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(optionsAction: (serviceProvider, dbContextOptionsBuilder) =>
+{
+    dbContextOptionsBuilder
+        .UseNpgsql(
+            serviceProvider.GetRequiredService<IConfiguration>().GetConnectionString(name: "Identity"), 
+            npgsqlOptionsAction: npgsqlDbContextOptionsBuilder => {
+                npgsqlDbContextOptionsBuilder.MigrationsAssembly(
+                    typeof(Program).GetTypeInfo().Assembly.GetName().Name);
+            });
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddClaimsPrincipalFactory<ApplicationUserClaimsPrincipleFactory>()
+    .AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+var app = builder.Build();
 
 app.Run();
